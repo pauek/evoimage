827748993ac8fc02f4133d84700095e12293944b
Enric Martí <enric@lsi11ct>
2009-03-11 14:43:07 +0100

    Millorada la GUI (minimament).
    He fet que acabi el proces quan es tanqui la finestra principal.
    I que quan es cliqui a resolucio del menu, doncs surti un quadre de 
    diàleg per setar-la. Ara faltaria destruir el widget setter si es clica 
    a Cancel, i com passar les variables entrades al programa en C!!!!!

--- pyevoimg/glade/prova1.py
+++ pyevoimg/glade/prova1.py
@@ -13,13 +13,35 @@ except:
 	sys.exit(1)
 
 class Prova1GTK:
-	def on_inicia_activate(self):
+	def on_inicia_activate(self , window):
 		print "inicia"
+
+	def on_atura_i_surt_activate(self , window):
+		gtk.main_quit()
+	
+	def on_resolucio_activate(self , window):
+        	self.setter.show()
+	
+	def gtk_widget_destroy(self , window):
+		print "esborrat"
+		gtk_widget_destroy()
+		
+	
+
+	def on_window1_destroy(self , window):
+		gtk.main_quit()
 
 	def __init__(self):
 		self.gladefile = "prova1.glade"  
-		self.wTree = gtk.glade.XML(self.gladefile, "window1")
-		dic = { "on_inicia_activate" : self.on_inicia_activate }
+		self.wTree = gtk.glade.XML(self.gladefile)
+		
+		self.setter = gtk.glade.XML.get_widget(self.wTree , "res_setter")
+		
+		dic = { "on_inicia_activate" : self.on_inicia_activate,
+			"on_atura_i_surt_activate" : self.on_atura_i_surt_activate,
+			"on_resolucio_activate" : self.on_resolucio_activate,
+			"getk_widget_destroy" : self.gtk_widget_destroy,
+			"on_window1_destroy" : self.on_window1_destroy }
 		self.wTree.signal_autoconnect(dic)
 
 if __name__ == "__main__":
