fccab3549b36358a063ee9cd83493ed1973f5d02
Pau Fernández <pau.fernandez@upc.edu>
2009-04-16 16:50:41 +0200

    Petita simplificació de "parseArgs" a "eval.cpp"

--- eval.cpp
+++ eval.cpp
@@ -13,23 +13,22 @@ int    width = 400;    // Amplada de la imatge
 int    height = 400;   // Alçada de la imatge
 string prefix = "img"; // Prefix del nom de fitxer
 
+int str2int(string s) {
+  double d;
+  stringstream sin(argv[++k]);
+  sin >> d;
+  return d;
+}
+
 void parseArgs(int argc, char *argv[], 
 	       int& width, int& height, string& prefix, 
 	       vector<string>& args) {
   int k = 1;
   while (k < argc) {
     string arg(argv[k]);
-    if (arg == "-o") {
-      prefix = string(argv[++k]);
-    } 
-    else if (arg == "-w") {
-      stringstream sin(argv[++k]);
-      sin >> width;
-    }
-    else if (arg == "-h") {
-      stringstream sin(argv[++k]);
-      sin >> height;
-    }
+    if (arg == "-o")      prefix = string(argv[++k]);
+    else if (arg == "-w") width  = str2int(argv[++k]);
+    else if (arg == "-h") height = str2int(argv[++k]);
     else {
       args.push_back(arg);
     }
