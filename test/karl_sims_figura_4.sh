85bacbb902b5b49d9f395af0a18639b862e41b44
Pau Fernández <pau.fernandez@upc.edu>
2009-04-16 23:08:59 +0200

    Afegeixo un test de la figura 4 del paper del Karl Sims

---
+++ test/karl_sims_figura_4.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+../eval -o 4a.pnm "x"
+../eval -o 4b.pnm "y"
+../eval -o 4c.pnm "(abs x)"
+../eval -o 4d.pnm "(mod x (abs y))"
+../eval -o 4e.pnm "(and x y)"
+../eval -o 4f.pnm "(bwNoise)"
+../eval -o 4g.pnm "(colorNoise)"
