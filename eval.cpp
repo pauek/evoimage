eda625c730bdfa79b0edeb9b758c1ecd899f6174
Pau Fernández <pau.fernandez@upc.edu>
2009-04-16 22:26:35 +0200

    Canvio el programa "eval" perquè només esperi una expressió

--- eval.cpp
+++ eval.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 // Paràmetres
-int    width = 400;    // Amplada de la imatge
-int    height = 400;   // Alçada de la imatge
-string prefix = "img"; // Prefix del nom de fitxer
+int    width = 400;        // Amplada de la imatge
+int    height = 400;       // Alçada de la imatge
+string outfile = "img.pnm"; // Nom de fitxer de sortida
 
 int str2int(string s) {
   double d;
@@ -18,13 +18,11 @@ int str2int(string s) {
   return d;
 }
 
-void parseArgs(int argc, char *argv[], 
-	       int& width, int& height, string& prefix, 
-	       vector<string>& args) {
+void parseArgs(int argc, char *argv[], vector<string>& args) {
   int k = 1;
   while (k < argc) {
     string arg(argv[k]);
-    if (arg == "-o")      prefix = string(argv[++k]);
+    if (arg == "-o")      outfile = string(argv[++k]);
     else if (arg == "-w") width  = str2int(argv[++k]);
     else if (arg == "-h") height = str2int(argv[++k]);
     else {
@@ -40,23 +38,19 @@ void usage() {
   cout << "options: " << endl;
   cout << " -w, image width [" << width << "]" << endl;
   cout << " -h, image height [" << height << "]" << endl;
-  cout << " -o, output file prefix [\"" << prefix << "\"]" << endl;
+  cout << " -o, output file [\"" << outfile << "\"]" << endl;
   cout << endl;
   exit(0);
 }
 
 int main(int argc, char *argv[]) {
   vector<string> args;
-  parseArgs(argc, argv, width, height, prefix, args);
-  if (args.empty()) usage();
+  parseArgs(argc, argv, args);
+  if (args.size() != 1) usage();
   
-  for (int k = 0; k < (int)args.size(); k++) {
-    stringstream sin(args[k]); 
-    Node* root = read(sin);
-    Image I(width, height);
-    root->eval(I);  
-    stringstream sout;
-    sout << prefix << k << ".pnm";
-    I.save_pnm(sout.str());
-  }
+  stringstream sin(args[0]); 
+  Node* root = read(sin);
+  Image I(width, height);
+  root->eval(I);  
+  I.save_pnm(outfile);
 }
